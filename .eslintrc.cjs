d3d48c6c41293b59054a0e94d7a625542356b258
Smbat1505 <1cor13a8@gmail.com>
2024-04-07 22:55:28 +0400

    Apply 'no-duplicate-imports' rule with warning level to TypeScript component files in 'src/assets/icons/components'

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,18 @@
 module.exports = {
-  extends: '@it-incubator/eslint-config',
+  extends: "@it-incubator/eslint-config",
   overrides: [
     {
-      files: ['**/*.stories.tsx'],
+      files: ["**/*.stories.tsx"],
       rules: {
-        'react-hooks/rules-of-hooks': 'off',
-        'no-console': 'off',
-      },
+        "react-hooks/rules-of-hooks": "off",
+        "no-console": "off"
+      }
     },
-  ],
-}
+    {
+      files: ["src/assets/icons/components/*.tsx"], // применить только к файлам с расширением .tsx
+      rules: {
+        "no-duplicate-imports": "warn"
+      }
+    }
+  ]
+};
