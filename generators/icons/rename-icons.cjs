e10f82feadb2182266a344de2ea7a4125d677a93
Smbat1505 <1cor13a8@gmail.com>
2024-04-08 08:01:42 +0400

    Implemented scripts for icon renaming and color replacement

---
+++ generators/icons/rename-icons.cjs
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const { join } = require("node:path");
+const fsp = fs.promises;
+
+const dirWithIcons = "src/assets/icons/svg";
+
+async function main() {
+  try {
+    const files = await fsp.readdir(dirWithIcons);
+    // files.forEach((file) => {
+    // const newName = file.replaceAll(' ', '-').replaceAll('(', '').replaceAll(')', '').toLowerCase()
+    // fsp.rename(join(dirWithIcons, file), join(dirWithIcons, newName));
+    // });
+
+    await Promise.all(files.map(async (file) => {
+      // Заменяем все пробелы и скобки на дефисы, а затем приводим к нижнему регистру
+      const newName = file.replace(/[\s()]/g, "-").toLowerCase();
+      await fsp.rename(join(dirWithIcons, file), join(dirWithIcons, newName));
+    }));
+
+    console.log("ALL FILES HAVE BEEN SUCCESSFULLY RENAMED");
+    console.log("Все файлы успешно переименованы");
+
+  } catch (error) {
+    console.error("AN ERROR HAS OCCURRED: ", error);
+    console.error("Произошла ошибка: ", error);
+  }
+
+}
+
+void main();
